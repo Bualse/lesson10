34b9ebf4c295f5bd02f32fccde794a83ea4801d4
budko <budkovalexandr@gmail.com>
2023-02-25 12:40:59 +0300

    на проверку с изменениями #2

--- src/Main.java
+++ src/Main.java
@@ -60,22 +60,27 @@ public class Main {
 
     public static void printPhoneVersion(int clientDeviceYear, int clientOs) {
         int currentYear = LocalDate.now().getYear();
+
         if (clientOs == 1) {
-            System.out.print("Ваша операционная система Android, ");
-        } else {
-            System.out.print("Ваша операционная система IOS, ");
-        }
-        if (clientDeviceYear < currentYear) {
-            System.out.println("установите облегченную версию приложения по ссылке");
-        } else {
-            System.out.println("установите обычную версию приложения по ссылке");
+            if (clientDeviceYear < currentYear) {
+                System.out.println("Ваша операционная система Android, установите облегченную версию приложения по ссылке");
+            } else {
+                System.out.println("Ваша операционная система Android, установите обычную версию приложения по ссылке");
+            }
+
+        } else if (clientOs == 0){
+            if (clientDeviceYear < currentYear) {
+                System.out.println("Ваша операционная система IOS, установите облегченную версию приложения по ссылке");
+            } else {
+                System.out.println("Ваша операционная система IOS, установите обычную версию приложения по ссылке");
+            }
         }
     }
 
     public static void task2() {
         System.out.println("Задача № 2.");
 
-        int clientOs = 1; // (0 — iOS, 1 — Android)
+        int clientOs = 0; // (0 — iOS, 1 — Android)
         int clientDeviceYear = 2023;
 
         printPhoneVersion(clientDeviceYear, clientOs);
@@ -84,8 +89,12 @@ public class Main {
     }
 
 
-    public static void printTotalDaysDelivery(int deliveryDistance) {
+    public static int calculateDaysDelivery(int deliveryDistance) {
         int deliveryDay = 1;
+
+        if (deliveryDistance > 100) {
+            return -1;
+        }
 
         if (deliveryDistance > 20) {
             deliveryDay++;
@@ -93,18 +102,21 @@ public class Main {
         if (deliveryDistance > 60) {
             deliveryDay++;
         }
-        if (deliveryDistance > 100) {
-            System.out.println("Доставки нет.");
-            return;
-        }
-        System.out.println("Потребуется дней для доставки: " + deliveryDay);
+        return deliveryDay;
     }
+
 
     public static void task3() {
         System.out.println("Задача № 3.");
 
-        int deliveryDistance = 102;
-        printTotalDaysDelivery(deliveryDistance);
+        int deliveryDistance = 170;
+        int result = calculateDaysDelivery(deliveryDistance);
+
+        if (result == -1) {
+            System.out.println("Доставки нет");
+        } else {
+            System.out.println("Потребуется дней для доставки: " + result);
+        }
 
         printSymbol();
     }
